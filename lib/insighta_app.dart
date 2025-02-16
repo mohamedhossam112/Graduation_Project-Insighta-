b3c228ad3e7dc0c8c55114c75ba6ddabf745b10c
Noha0212 <nonathabit2002@gmail.com>
2025-02-16 23:37:23 +0200

    add core folders & some packages

---
+++ lib/insighta_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InsightaApp extends StatelessWidget {
+  const InsightaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:insighta/main.dart';
+import 'package:insighta/insighta_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const InsightaApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
